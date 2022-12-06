d84ee63ae964aade68af639fa9b6b9ddfc89a922
ahmed-debbech <debbech.ahmed@gmail.com>
2022-12-06 11:50:08 +0100

    solve day6 part1

---
+++ day6/part1.js
@@ -0,0 +1,20 @@
+const fs = require('fs')
+
+let file = fs.readFileSync('input', 'utf8');
+
+function isUnique(str){
+    for(let i = 0; i < str.length; i++)
+        for(let j = i + 1; j < str.length; j++)
+            if (str[i] == str[j])
+                return false;
+
+    return true;
+}
+
+for(let i=0; i<=file.length-1; i++){
+    let subst = file.substring(i, i+4)
+    if(isUnique(subst)){
+        console.log("THE RESULT: " + (i+4));
+        break;
+    }
+}
